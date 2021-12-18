3a0346b88fe4b62afeef4d6e5636c1e9673399a5
jackbigs <jacopo.grandi@gmail.com>
2021-12-18 19:01:01 +0100

    lazy algorithm

--- day_1/src/sonar.py
+++ day_1/src/sonar.py
@@ -1,30 +1,18 @@
 import sys
 
 def read_input(path : str) -> list:
-    input = []
     with open(path, "r") as input_file:
-        lines = input_file.readlines()
-        for ln in lines:
-            input.append(int(ln))
-    return input
-
-def count_rise(input : list, output_path : str) -> int:
-    with open(output_path, "w") as output:
-        previous = None
-        count = 0
-        for value in input:
-            if previous is not None:
-                if previous < value:
-                    msg = str(previous) + " < " + str(value) + "\n"
-                    output.write(msg)
-                    count += 1
-            previous = value
-        output.write("total: " + str(count))
-    return count
+        return input_file.readlines()
 
 def main():
-    input = read_input(sys.argv[1])
-    count = count_rise(input, sys.argv[2])
+    lines = read_input(sys.argv[1])
+    input = (int(ln) for ln in lines)
+    count = 0
+    previous = None
+    for value in input:
+        if previous is not None and previous < value:
+            count +=1
+        previous = value
     print(count)
 
 if __name__ == "__main__":
