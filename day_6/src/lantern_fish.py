ea2ec02cef49d3a085bc4012b35691859ceb75a7
jackbigs <jacopo.grandi@gmail.com>
2022-04-03 20:11:43 +0200

    day 6: wip

--- day_6/src/lantern_fish.py
+++ day_6/src/lantern_fish.py
@@ -15,23 +15,29 @@ def print_fishes(epoch : int, fishes : list) -> None:
         msg += str(fish) + ", "
     print(msg)
 
+def group(fishes : list, max_count : int) -> list:
+    groups = [0 for i in range(0, max_count)]
+    for index in fishes:
+        assert(index < max_count)
+        groups[index] += 1
+    return groups
+
 def main():
-    start_counter = 8
+    max_counter = 8
     restart_counter = 6
     epoch_count = int(sys.argv[2])
     fishes = read_input(sys.argv[1]) 
+    groups = group(fishes, max_counter + 1)
     for epoch in range(0, epoch_count):
-        new_fishes = []
-        for i in range(0, len(fishes)):
-            #print(fs)
-            if fishes[i] == 0:
-                fishes[i] = restart_counter
-                new_fishes.append(start_counter)
-            else:
-                fishes[i] -= 1
-        fishes += new_fishes
+        new_fishes = groups[0]
+        for i in range(1, len(groups)):
+            groups[i - 1] = groups[i]
+        groups[len(groups) - 1] = new_fishes
+        # the ones that reach 0 restart from restart_counter
+        groups[restart_counter - 1] += new_fishes 
+        #print(groups)
     #print_fishes(epoch_count, fishes)
-    print("Fishes: " + str(len(fishes)))
+    print("Fishes: " + str(sum(groups)))
 
 if __name__ == "__main__":
     main()
